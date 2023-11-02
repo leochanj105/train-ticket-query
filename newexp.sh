dc272d0c2851685a74ce8ea1a0165daf4f29bf43
leochanj <leochanj@node1.throughput.lumos-pg0.utah.cloudlab.us>
2023-11-01 20:10:49 -0600

    new exp scripts

---
+++ newexp.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -u
+
+direc=$1
+mint=$2
+maxt=$3
+step=$4
+reqpt=$5
+
+loginres=($(python3 login.py))
+aid=${loginres[0]}
+token=${loginres[1]}
+
+python3 req.py -1 5 xx $aid $token
+
+echo "doing for #clients=1"
+python3 req.py 1 $reqpt $direc $aid $token
+
+for i in $(seq $mint $step $max)
+do	
+	echo "doing for #clients="$i
+	python3 req.py $i $reqpt $direc $aid $token
+done
+
+
+
