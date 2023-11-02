9f735577e1d95e3582bbf282166b21e968a6ba66
leochanj <leochanj@node1.throughput.lumos-pg0.utah.cloudlab.us>
2023-11-01 20:35:44 -0600

    fix typo

--- newexp.sh
+++ newexp.sh
@@ -13,10 +13,10 @@ token=${loginres[1]}
 
 python3 req.py -1 5 xx $aid $token
 
-echo "doing for #clients=1"
-python3 req.py 1 $reqpt $direc $aid $token
+#echo "doing for #clients=1"
+#python3 req.py 1 $reqpt $direc $aid $token
 
-for i in $(seq $mint $step $max)
+for i in $(seq $mint $step $maxt)
 do	
 	echo "doing for #clients="$i
 	python3 req.py $i $reqpt $direc $aid $token
