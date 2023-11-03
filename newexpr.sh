74232c374c8c01379025d167364c543e52b65e76
leochanj <leochanj@node1.throughput.lumos-pg0.utah.cloudlab.us>
2023-11-03 12:05:37 -0600

    changed newexpr.sh

--- newexpr.sh
+++ newexpr.sh
@@ -2,26 +2,28 @@
 set -u
 
 direc=$1
-mint=$2
-maxt=$3
-step=$4
-reqpt=$5
+#mint=$2
+#maxt=$3
+#step=$4
+reqpt=$2
 
 loginres=($(python3 login.py))
 aid=${loginres[0]}
 token=${loginres[1]}
 
-python3 req.py -1 5 xx $aid $token
+#python3 req.py -1 5 xx $aid $token
 
-echo "doing for #clients=1"
-dirname="$direc/c1r${reqpt}"
-python3 req.py 1 $reqpt $dirname $aid $token
+#echo "doing for #clients=1"
+#dirname="$direc/c1r${reqpt}"
+#python3 req.py 1 $reqpt $dirname $aid $token
 
-for i in $(seq $mint $step $maxt)
+#for i in $(seq $mint $step $maxt)
+while IFS= read -r line;
 do	
-	dirname="$direc/c${i}r${reqpt}"
-	echo "doing for #clients="$i, $dirname
-	python3 req.py $i $reqpt $dirname $aid $token
+	echo $line
+	dirname="$direc/c${line}r${reqpt}"
+	echo "doing for #clients="$line, $dirname
+	python3 req.py $line $reqpt $dirname $aid $token
 done
 
 
