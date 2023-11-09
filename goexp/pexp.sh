05a75ded3a67bd4d3d7b0860a74a9562e218769d
leochanj <leochanj@node1.tput.lumos-pg0.utah.cloudlab.us>
2023-11-09 08:48:04 -0700

    results

--- goexp/pexp.sh
+++ goexp/pexp.sh
@@ -2,12 +2,20 @@ echo "warmup"
 nreqs=$1
 dname=$2
 
-bash pay.sh 30 30 10 wup < /dev/null
+bash pay.sh 30 30 100 wup < /dev/null
 while IFS=" " read -r nprocs nthds;
 do
         #dirname="$direc/c${line}r${reqpt}"
         echo "doing for "$nprocs, $nthds
-        bash pay.sh $nprocs $nthds $nreqs $dname < /dev/null
+	total=$(($nprocs * $nthds))
+	actual=$nreqs
+	if [ $total -lt 101 ]
+	then
+		actual=$(($nreqs * 2))
+		echo $actual
+	fi
+	
+        bash pay.sh $nprocs $nthds $actual $dname < /dev/null
 	echo "done"
 done
 
